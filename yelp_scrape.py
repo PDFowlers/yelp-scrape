0518f32b47f54945e85c09bda8f91167c81ebf3e
Paul <pauldf5@gmail.com>
2022-06-04 15:30:36 -0400

    .py start

--- yelp_scrape.py
+++ yelp_scrape.py
@@ -4,3 +4,8 @@ from bs4 import BeautifulSoup
 # this project is aimed at searching yelp for various good and services and providing a "top 5" list of
 # recommendations to the user
 
+# get_search_request will prompt the user for a search item and a location and return those as strings we can use
+# to scrape the correct webpages
+
+def get_search_request():
+    pass
