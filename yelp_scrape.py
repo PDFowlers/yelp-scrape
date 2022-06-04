c5ef1dffca0d3836e31e3f87ae7b8d5d47719fd4
Paul <pauldf5@gmail.com>
2022-06-04 15:14:28 -0400

    First commit

---
+++ yelp_scrape.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+# this project is aimed at searching yelp for various good and services and providing a "top 5" list of
+# recommendations to the user
+
