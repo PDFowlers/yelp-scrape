a389cd0a5ca9e7d97d6d46a98dfeedc07d4c40e2
Paul <pauldf5@gmail.com>
2022-06-07 16:18:15 -0400

    cache branch edit 1; testing click functionality and hash repetition

---
+++ hash_assertion_test.py
@@ -0,0 +1,18 @@
+
+from cgi import test
+
+
+def hash_test(test_input: str) ->str:
+    hash_value: int = hash(test_input)
+    hex_str: str = hex(hash_value)
+    return hex_str 
+
+def hash_assertion():
+    a = 'test'
+    b = 'test'
+    print(hash_test(a))
+    print(hash_test(b))
+    assert(hash_test(a) == hash_test(b))
+
+
+print(hash_assertion())
