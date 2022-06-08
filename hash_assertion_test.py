f2a299107cb515f816751af31aaca90f8bdc4024
Uiotomon <pfowler@jerviswebb.com>
2022-06-08 12:38:45 -0400

    no such command errors

--- hash_assertion_test.py
+++ hash_assertion_test.py
@@ -7,12 +7,11 @@ def hash_test(test_input: str) ->str:
     hex_str: str = hex(hash_value)
     return hex_str 
 
-def hash_assertion():
-    a = 'test'
-    b = 'test'
+def hash_assertion(a, b):
     print(hash_test(a))
     print(hash_test(b))
     assert(hash_test(a) == hash_test(b))
 
 
-print(hash_assertion())
+print(hash_assertion('test', 'test'))
+print(hash_assertion('test', 'test'))
--- yelp_scrape.py
+++ yelp_scrape.py
@@ -10,34 +10,42 @@ import click
 
 # get_search_request will prompt the user for a search item and a location and return those as strings we can use
 # to scrape the correct webpages
+@click.group(chain=True)
+def cli():
+    pass
 
 # def get_search_request():
-#     search_item = click.echo(f'{search_item}')
+#     search_item = input(f'Please input a good or service you would like recommendations for: ')
 #     sleep(0.5)
-#     location = click.echo(f'{location}')
+#     location = input('Please input the location you would like to search, with each item separated by'
+#                      ' a comma. (ex. Detroit, Michigan, 48127): ')
 #     return search_item, location
 
+# help = 'search query for Yelp.com. Ex. "chinese food"'
+# help = 'location is the area you would like to search such as city or postal code'
 # url_generator will take the outputs of get_search_request and use them to generate a url that can then be used
 # by requests.get
 @click.command()
-@click.option('--search_item', type = str, prompt = 'Please input the good or service you would like recommendations for: ', help = 'search query for Yelp.com. Ex. "chinese food"')
-@click.option('--location', type = str, prompt = 'Please input the location you would like to search, with each item separated by'
-                     ' a comma. (ex. Detroit, Michigan, 48127): ', help = 'location is the area you would like to search such as city or postal code')
-def url_generator(search_item, location):
+@click.argument('search_item')
+@click.argument('location')
+def url_generator(search_item: str, location: str) ->str:
     base_url = 'https://www.yelp.com/search?'
     search_item = str(search_item).replace(' ', '+')
     location = str(location).replace(' ', '+').replace(',', '%2C')
     url = base_url + 'find_desc=' + search_item + '&find_loc=' + location
-    file_name = search_item + location
+    file_name = search_item + location + '.html'
     return url, file_name
 
 # local_cache_check will take the search request url and check a local directory for a file of the same name
 # if the file is found then it will use that file instead of scraping the internet
-def local_cache_check(url: str, file_name: str | Path):
+@click.command()
+@click.argument('file_name', type = click.Path(exists=True))
+def local_cache_check(url: str, file_name: str | Path) -> BeautifulSoup:
     # url_hash = hash(url)
-    path = str(click.option('--filepath', prompt = 'Please enter complete filepath to search: '))
+    # path = input('Please enter complete filepath to search: ')
+    print('check')
+    path = file_name
     file_list = os.listdir(f'{path}')
-    file_name = file_name + '.html'
     file_path = os.path.join(path, file_name)
     if file_name in file_list:
         print('Retrieving the file from the directory')
@@ -48,10 +56,18 @@ def local_cache_check(url: str, file_name: str | Path):
         yelp_page = requests.get(url)
         yelp_soup = BeautifulSoup(yelp_page.content, 'html.parser')
         with open(file_path, 'w') as file:
-            file.write(yelp_soup.prettify())
+            file.write(yelp_soup)
     return yelp_soup
 
 
-yelp_soup = local_cache_check(*url_generator())
 
-print(yelp_soup)
+
+
+
+cli.add_command(url_generator)
+cli.add_command(local_cache_check)
+
+if __name__ == "__main__":
+    cli()
+
+
